254f9f53f40df4db21c4a2bd84f28698037bab95
Jojnas <jan.stancek49@gmail.com>
2017-01-27 08:25:31 +0100

    v4

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,27 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anyone';
+var room = getQueryVariable('room');
+
+jQuery('.room-title').text(room);
 
 socket.on('connect', function () {
     console.log('Connected to socket.io server');
+    socket.emit('joinRoom', {
+       name: name,
+       room: room
+    });
 });
 
 socket.on('message', function (hovnocuc) {
     var momentTimeStamp = moment.utc(hovnocuc.timestamp);
+    var $message = jQuery('.messages');
+
+
+
     console.log('New message: ' + hovnocuc.text);
 
-    jQuery('.messages').append('<p><strong>' + momentTimeStamp.local().format('h:mm a') + '</strong>: ' +  hovnocuc.text + '</p>');
+    $message.append('<p><strong>' + hovnocuc.name + ' ' + momentTimeStamp.local().format('h:mm a') + ': ' + '</strong></p>');
+    $message.append('<p>' + hovnocuc.text + '</p>');
 });
 
 // handles submitting of new message
@@ -19,8 +32,10 @@ $form.on('submit', function (event) {
     var $message = $form.find('input[name=message]');
 
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
 
     $message.val('');
 });
+
--- server.js
+++ server.js
@@ -8,21 +8,77 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if (typeof info === 'undefined') {
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function (socketId) {
+        var userInfo = clientInfo[socketId];
+        if(info.room === userInfo.room) {
+            users.push(userInfo.name);
+        }
+    });
+
+    socket.emit('message', {
+       name: 'System',
+       text: 'Current users: ' + users.join(', '),
+       timestamp: moment().valueOf()
+    });
+}
+
+
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
+
+    socket.on('disconnect', function () {
+        var userData = clientInfo[socket.id];
+        if (typeof  userData !== undefined) {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+               name: 'System',
+               text: userData.name + ' has left!',
+               timestamp: moment().valueOf()
+            });
+            delete userData;
+        }
+    });
+
+    socket.on('joinRoom', function (req) {
+        clientInfo[socket.id] = req;
+        // vstavana metoda
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message', {
+            name: 'System',
+            text: req.name + ' has joined!',
+            timestamp: moment().valueOf()
+        });
+    });
 
     socket.on('message', function (hovnocuc) {
         console.log('Message received: ' + hovnocuc.text);
 
-        // toto posiela spravu vsetkym, okrem odosielatela
-        // socket.broadcast.emit('message', hovnocuc);
+        if (hovnocuc.text === '@currentUsers') {
+            sendCurrentUsers(socket);
+        } else {
 
-        // toto aj odosielatovi
-        hovnocuc.timestamp = moment().valueOf();
-        io.emit('message', hovnocuc);
+            // toto posiela spravu vsetkym, okrem odosielatela
+            // socket.broadcast.emit('message', hovnocuc);
+            hovnocuc.timestamp = moment().valueOf();
+            // toto aj odosielatovi
+            io.to(clientInfo[socket.id].room).emit('message', hovnocuc);
+        }
+
     });
 
     socket.emit('message', {
+        name: 'System',
         text: 'Welcome to the chat application!',
         timestamp: moment().valueOf()
     });
